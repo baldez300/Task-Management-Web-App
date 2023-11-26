d58bc0646cfb3737301fce582140283f87040832
baldez300 <baldez111990@gmail.com>
2023-11-27 00:56:00 +0200

    Implemented dynamic generation of 'Edit' and 'Delete' task links in the navigation menu. Updated 'Get Task by Id' link to be dynamic. Enhanced task-related templates to handle dynamic task data. Reverted previous commit to resolve form data access issues. Cleaned up and optimized codebase.

--- app/routes.py
+++ app/routes.py
@@ -109,7 +109,7 @@ def create_new_task():
 @login_required
 def get_task_by_id(task_id):
     task = Task.query.get_or_404(task_id)
-    return render_template('get_task_byId.html', task=task)
+    return render_template('get_task_by_id.html', task=task)
 
 @blueprint.route('/tasks/<int:task_id>/edit', methods=['GET', 'POST'])
 @login_required
